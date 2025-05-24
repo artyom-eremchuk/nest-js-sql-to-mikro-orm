ed6e4e140643e3ea58f9bb3869fd83195c5a8c93
Artyom Eremchuk <aeremchuk.dev@yandex.ru>
2025-05-24 20:13:46 +0300

    feat: add swagger, change express to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { Logger } from '@nestjs/common';
 
 const logger = new Logger('MainLogger');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SQL to MikroORM API')
+    .setDescription('API for converting native SQL implementations to MikroORM')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('documentation', app, swaggerDocument);
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 
 bootstrap()
